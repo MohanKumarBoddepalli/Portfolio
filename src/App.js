1f8ab16e32079aec4f6b787ab9b61f165084671e
Mohan Kumar Boddepalli <126196778+MohanKumarBoddepalli@users.noreply.github.com>
2023-12-25 23:56:06 +0530

    alignment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='grid grid-cols-1 gap-10'>
       <Profile />
-      <div className='flex flex-col gap-10 lg:px-10 p-4'>
+      <div className='flex flex-col gap-10 p-4'>
         <Skills />
         <Experience />
       </div>
