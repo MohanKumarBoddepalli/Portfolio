e3cd60d7b88af44b9c37a89546181c37aab2797b
Mohan Kumar Boddepalli <126196778+MohanKumarBoddepalli@users.noreply.github.com>
2024-03-11 00:04:20 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import Footer from './component/footer';
 
 function App() {
   return (
-    <div className='grid grid-cols-1 gap-10'>
+    <>
+    <div className='grid grid-cols-1 gap-10 content-baseline min-h-screen'>
       <Profile />
-      <div className='flex flex-col gap-10 p-4 min-h-screen'>
+      <div className='flex flex-col gap-10 p-4'>
         <Skills />
         <Experience />
       </div>
+    </div>
       <footer>
         <Footer />
       </footer>
-    </div>
+    </>
   );
 }
 
