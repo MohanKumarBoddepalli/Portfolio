411bd72362b61bb0624ae9de09661d8b47ca5489
Mohan Kumar Boddepalli <126196778+MohanKumarBoddepalli@users.noreply.github.com>
2024-03-10 21:47:24 +0530

    responsive

--- src/component/Experience/index.js
+++ src/component/Experience/index.js
@@ -3,14 +3,14 @@ import experience from './Experience';
 const ImagedComponent = (path, altDesc) => {
 
   return (
-    <img className={`flex h-48 drop-shadow-[0_35px_35px_rgba(24,102,119,0.25)]`} alt={''} src={path}></img>
+    <img className={`flex xs:h-24 lg:h-32 md:h-48 items-center justify-center alidrop-shadow-[0_35px_35px_rgba(24,102,119,0.25)]`} alt={''} src={path}></img>
   )
 }
 const details = (heading, desc) => {
   return (
     <div className='flex flex-col rounded-md '>
       <div className='p-2'>{heading}</div>
-      <div className='gap-2 p-4 text-slate-400'>{desc}</div>
+      <div className='gap-2 xs:text-xs sm:text-sm lg:text-lg text-slate-400'>{desc}</div>
     </div>
   )
 }
@@ -24,18 +24,18 @@ const Experience = () => {
       </div>
       <div className='flex flex-col w-full justify-center place-items-center items-center'>
         {experience.map((data) =>
-          <div className="flex w-full xl:w-3/5  grid-cols-3 size-full gap-2 items-center h-56 gap-x-10 justify-center place-items-center">
-            <div className="flex pr-1.5 justify-center lg:justify-end md:justify-end xl:justify-end 2xl:justify-end w-full items-center h-56">
+          <div className="flex w-full xl:w-3/5  grid-cols-3 size-full gap-2 items-center h-4/5 gap-x-10 justify-center place-items-center">
+            <div className='flex p-2 pl-1.5 w-full items-center justify-center lg:justify-start md:justify-start xl:justify-start  h-4/5'>
               {data.leftImage ? ImagedComponent(data.right, data.mode) : details(data.heading, data.desc)}
             </div>
-            <div className=" w-px h-56 justify-center items-center flex bg-gray-200">
+            <div className=" w-px h-full justify-center items-center flex bg-gray-200">
               <div className="flex justify-center w-11 h-11 rounded-full bg-cyan-900 absolute items-center bg-gradient-to-r from-cyan-500 to-blue-500">
                 <div className=" items-center justify-center">
                   {data.year}
                 </div>
               </div>
             </div>
-            <div className='flex pl-1.5 w-full items-center justify-center lg:justify-start md:justify-start xl:justify-start  h-56'
+            <div className='flex p-2 pl-1.5 w-full items-center justify-center lg:justify-start md:justify-start xl:justify-start  h-4/5'
             >
               {data.leftImage ? details(data.heading, data.desc) : ImagedComponent(data.right, data.altDesc)}
             </div>
--- tailwind.js
+++ tailwind.js
@@ -11,6 +11,12 @@ module.exports = {
       ...theme('colors'),
       auto: 'auto',
     }),
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
     animation: {
       none: 'none',
       spin: 'spin 1s linear infinite',
