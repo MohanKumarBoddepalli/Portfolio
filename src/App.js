312eb1955399eb594b1df06bb751b3b9b733a010
Mohan Kumar Boddepalli <126196778+MohanKumarBoddepalli@users.noreply.github.com>
2024-03-11 00:11:59 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './assets/main.css'
-import './App.css';
-import Profile from './component/profile/index'
-import Skills from './component/skills';
-import Experience from './component/Experience'
-import Footer from './component/footer';
+import "./assets/main.css";
+import "./App.css";
+import Profile from "./component/profile/index";
+import Skills from "./component/skills";
+import Experience from "./component/Experience";
+import Footer from "./component/footer";
 
 function App() {
   return (
-    <>
-    <div className='grid grid-cols-1 gap-10 content-baseline min-h-screen'>
-      <Profile />
-      <div className='flex flex-col gap-10 p-4'>
-        <Skills />
-        <Experience />
+    <div className="min-h-screen">
+      <div className="grid grid-cols-1 gap-10 min-h-[95vh] content-baseline">
+        <Profile />
+        <div className="flex flex-col gap-10 p-4">
+          <Skills />
+          <Experience />
+        </div>
       </div>
-    </div>
-      <footer>
+      <footer className="min-h-[5vh]">
         <Footer />
       </footer>
-    </>
+    </div>
   );
 }
 
